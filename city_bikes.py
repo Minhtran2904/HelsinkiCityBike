7f1e7777a7d3a22cb16b33fee2b215e8ed0c2b1c
Minhtran2904 <97359403+Minhtran2904@users.noreply.github.com>
2023-05-26 16:05:14 +0300

    Update city_bikes.py
    added greatest_distance between 2 stations, changed for readability

--- city_bikes.py
+++ city_bikes.py
@@ -42,3 +42,25 @@ def longest_distance(stations: dict, stations_list: list, station: str):
             greatest_tuple = (station, compare, d)
             greatest = d
     return greatest_tuple
+
+def greatest_distance(stations: dict):
+    stations_list = []
+    for key, value in stations.items():
+        stations_list.append(key)
+
+    greatest_tuple = None
+    greatest = 0
+    for index, station in enumerate(stations_list):
+        if index < len(stations_list)-1:
+            stations_distance = longest_distance(stations, stations_list, station)
+            station1, station2, distance = stations_distance
+            if distance > greatest:
+                greatest_tuple = stations_distance
+                greatest = distance
+    
+    return greatest_tuple
+
+if __name__ == "__main__":
+	stations = get_station_data('stations1.csv')
+	station1, station2, greatest = greatest_distance(stations)
+	print(station1, station2, greatest)
