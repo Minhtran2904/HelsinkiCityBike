753e563a99ab2428189c589eb793065aa0038205
Minhtran2904 <97359403+Minhtran2904@users.noreply.github.com>
2023-05-26 16:03:55 +0300

    Update city_bikes.py
    Redo readfile function, added get_station_data()

--- city_bikes.py
+++ city_bikes.py
@@ -8,3 +8,14 @@ def read_file(filename: str):
                 continue
             lines.append(line)
         return lines
+
+    def get_station_data(filename: str):
+    stations_info = read_file(filename)
+    stations = {}
+    for station in stations_info:
+        station_data = station.split(';')
+        name = station_data[3]
+        long = float(station_data[0])
+        lat = float(station_data[1])
+        stations[name] = (long, lat)
+    return stations
