e850761e7e6edec013a050ea9f1dffb66512f768
Minhtran2904 <97359403+Minhtran2904@users.noreply.github.com>
2023-05-26 16:04:30 +0300

    Update city_bikes.py
    commit longest_distance() between 2 stations

--- city_bikes.py
+++ city_bikes.py
@@ -19,3 +19,26 @@ def read_file(filename: str):
         lat = float(station_data[1])
         stations[name] = (long, lat)
     return stations
+
+import math
+def distance(stations: dict, station1: str, station2: str):
+    long1, lat1 = stations[station1]
+    long2, lat2 = stations[station2]
+
+    x_km = (long1 - long2) * 55.26
+    y_km = (lat1 - lat2) * 111.2
+    distance_km = math.sqrt(x_km**2 + y_km**2)
+
+    return distance_km
+
+def longest_distance(stations: dict, stations_list: list, station: str):
+    start_index = stations_list.index(station)+1
+    compare_list = stations_list[start_index:]
+    greatest_tuple = None
+    greatest = 0
+    for compare in compare_list:
+        d = distance(stations, station, compare)
+        if d > greatest:
+            greatest_tuple = (station, compare, d)
+            greatest = d
+    return greatest_tuple
